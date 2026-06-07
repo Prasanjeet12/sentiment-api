fc74b581acc21be86a1c9fb259926252dbd7fa7f
Prasanjeet12 <prasanjeetkumar2@gmail.com>
2026-06-07 16:08:24 +0530

    Add CORS support

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SentimentRequest(BaseModel):
     sentences: list[str]
